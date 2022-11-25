0bb8d936021fb89f3615bf74b4515ac0dedda89a
Uriel Zacarias <urielzacarias@gmail.com>
2022-11-24 22:38:21 -0800

    updated Ecommerce page with useful charts to display data

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -41,7 +41,7 @@ module.exports = {
         },
         backgroundImage: {
           'hero-pattern':
-            "url('https://i.ibb.co/MkvLDfb/Rectangle-4389.png')",
+            "url('./data/product2.jpg')",
         },
       },
     },
