d1cdd5291af4165a79f8e64f71ce0819deb29a19
Uriel Zacarias <urielzacarias@gmail.com>
2022-11-24 23:55:29 -0800

    added color theme selection

--- client/src/App.js
+++ client/src/App.js
@@ -41,7 +41,7 @@ const App = () => {
 
             </TooltipComponent>
           </div>
-          {activeMenu ? (
+          {activeMenu===true ? (
             <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white ">
               <Sidebar />
             </div>
@@ -53,8 +53,8 @@ const App = () => {
           <div
             className={
               activeMenu
-                ? 'dark:bg-main-dark-bg  bg-main-bg min-h-screen md:ml-72 w-full  '
-                : 'bg-main-bg dark:bg-main-dark-bg  w-full min-h-screen flex-2 '
+                ? 'dark:bg-main-dark-bg  bg-main-bg min-h-screen md:ml-72 w-full'
+                : 'bg-main-bg dark:bg-main-dark-bg  w-full min-h-screen flex-2'
             }
           >
             <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full ">
